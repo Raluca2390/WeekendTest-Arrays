f7bf95ab4ce97abc197039e3491f1113901b2a2b
Serbanescu <ralucang@gmail.com>
2017-02-26 20:41:16 +0200

    Sum of N numbers

---
+++ src/SumOfNnumbers.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Serbanescu on 26.02.2017.
+ */
+public class SumOfNnumbers {
+    public static void main(String[] args) {
+        int n = 20;
+        int sum;
+        sum = n*(n + 1) / 2;
+
+        System.out.println("Sum of first n number is: " + sum);
+
+    }
+}
