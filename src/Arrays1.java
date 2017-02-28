3fabd38b4d0652928965a86eadce9a5ec2f20158
Serbanescu <ralucang@gmail.com>
2017-02-28 14:17:52 +0200

    Siruri - array give the number of the numbers from the

---
+++ src/Arrays1.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Serbanescu on 28.02.2017.
+ */
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+
+        numbers[4] = 5;
+        numbers[3] = 8;
+        numbers[2] = 9;
+        numbers[0] = 1;
+        numbers[1] = 5;
+
+       // int[] nums = {1, 5, 6, 3, 7, 9};
+        System.out.println(numbers[4]);
+    }
+}
