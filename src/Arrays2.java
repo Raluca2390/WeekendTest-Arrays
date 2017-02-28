711e0c2374b552b1d82ab962af38aa9e00d6491b
Serbanescu <ralucang@gmail.com>
2017-02-28 14:27:25 +0200

    Siruri - array give the numbers in string

---
+++ src/Arrays2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Serbanescu on 28.02.2017.
+ */
+public class Arrays2 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 5, 6, 3, 7, 9};
+        System.out.println(numbers[5]);
+    }
+}
