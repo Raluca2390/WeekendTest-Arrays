71eea72a823a6e8408c4327226267a214287661c
Serbanescu <ralucang@gmail.com>
2017-02-28 19:51:29 +0200

    fsdf

---
+++ src/NNamSurname.java
@@ -0,0 +1,30 @@
+/**read N first name&surname combination from keyboard; write surname=firts name&length of surname"popescu+victoria7"
+ * Created by Serbanescu on 28.02.2017.
+ */
+public class NNamSurname {
+ /*   public static void main(String[] args) {
+       // int n = SkeletonJava.readIntConsole("N first n&s");
+        System.out.println("nume:");
+        String name = SkeletonJava.readStringConsole("") ;
+        System.out.println("prenume:");
+        String surname = SkeletonJava.readStringConsole((""));
+
+        System.out.println(name+" " +surname);
+
+
+    }*/
+
+    public static void main(String[] args) {
+        int persoane = SkeletonJava.readIntConsole("cate persoane sa fie");
+        String[] names = new String[persoane] ;
+        String[] surname = new String[surname];
+            for(int i=0; i< persoane; i++){
+                 names[i] = SkeletonJava.readStringConsole("nume") ;
+                 surname[i] = SkeletonJava.readStringConsole(("prenume"));
+            }
+            for(int i=0; i<persoane; i++){
+                SkeletonJava.printConsole(names[i] +"+" +surname[i] + names[i].length());
+            }
+    }
+
+}
